db41fd6b9035e251ddebaac7d8317f3e0f4124bc
guardslan <56944885+guardslan@users.noreply.github.com>
2020-03-03 11:48:01 +0800

    Add files via upload

---
+++ 20200303.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 商用程式設計
+# chop 0
+# * hello world
+# * a+b
+# * requests
+
+# In[ ]:
+
+
+print('hellow world')
+
+
+# In[ ]:
+
+
+
+
+
+# In[25]:
+
+
+
+a=5
+b=5
+
+int=c=a/b
+print(c)
+
+
+# In[28]:
+
+
+import requests
+a=requests.get('http://www.google.com/')
+print(a.text)
+
+
+# In[ ]:
+
+
+
+
